9c8a5a20ce42ee1c7f31acf238d33d7105103c4f
Alan Vekselman <avekselman@itba.edu.ar>
2021-02-23 04:14:18 -0300

    Moved repository

--- compile.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\alanv\PycharmProjects\Server\templates
-
-pyinstaller main2.py --onefile --hidden-import="pynput.keyboard._win32" --hidden-import="pynput.mouse._win32" -n "Remote_Keyboard.exe"
